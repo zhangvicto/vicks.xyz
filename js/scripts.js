ef528f539792f8821b6ea0bc1b6cdd6b4d98dde5
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2020-05-12 02:01:34 -0400

    Fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 //changing title as enter is pressed
 var input = document.getElementById("userInput");
+function btnAction (){
+    document.title = "Victor Zhang";
+    //reveal new page
+    document.getElementById("main").style.display = "block";
+}
 
+//key pressed action
 input.addEventListener("keydown",function(press){
     if (press.keyCode === 13) {
         press.preventDefault();
-        document.title = "Victor Zhang";
-        document.getElementById()
+        btnAction();
     }})
