7b985fd8542cbe52776ee0d19d4a0fe87680e80d
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2020-05-12 01:13:07 -0400

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 //changing title as enter is pressed
 let clickEnter = false;
-if {
-    document.getElementById(title) = "Victor Zhang";
+let input = document.getElementById("i");
+input.addEventListener("keyup",function(press){
+    if (press.keyCode === 13) {
+        clickEnter = true;
+    }})
+
+let t = document.title;
+console.log(t);
+if (clickEnter = true) {
+    t = "Victor Zhang";
 }
