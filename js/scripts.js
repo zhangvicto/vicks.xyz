b2cc97888b4d9427fced327260fa37a7541fccb3
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2020-05-15 00:47:26 -0400

    Update to front page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 //changing title as enter is pressed
 var input = document.getElementById("userInput");
 function btnAction (){
+
     //change title
     document.title = "Victor Zhang";
 
@@ -8,14 +9,36 @@ function btnAction (){
     document.getElementById("main").style.display = "block";
     document.getElementById("btn1").style.display = "none";
     document.getElementById("content").style.display = "none";
+
+    //add border to footer
+    document.getElementById('foot').style.borderTop = "2px solid white";
+
+    //add contact to footer
+    document.getElementById('foot').innerHTML = "<div style="padding:10px;"> Vicks<strong>Dev</strong> 2020 <strong><a href="mailto:vzhang24@uwo.ca"
+    style="text-decoration: none;color:white;">Contact</a></strong></div>";
+
+    //disable parallax running in background
+    parallax.disable();
+
+    //create new parallax for main
+    var scene2 = document.getElementById('main');
+    var parallax2 = new Parallax(scene2, {relativeInput:true});
+
 }
 
 //key pressed action
 input.addEventListener("keydown",function(press){
     if (press.keyCode === 13) {
         press.preventDefault();
-        btnAction();
-    }})
+
+        //trigger same effects as hover
+        document.getElementById("btn1").style.transform = "scale(0.8)";
+        document.getElementById("btn1").style.boxShadow = "none";
+        document.getElementById("btn1").style.transitionDuration = "400ms";
+
+        //time delay for hover effect to show
+        setTimeout(function(){btnAction()}, 400);
+    }});
 
 
 //hover on box to show animation and text change
