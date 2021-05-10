4d272d6d886c8231c4b32a65aa39d4c8498119c7
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2021-05-09 23:58:40 -0400

    backup

--- new/js/script.js
+++ new/js/script.js
@@ -8,15 +8,96 @@ menuHover.onmouseleave = function(){
     topMarginUp(document.getElementById("menu-hover"), 25, 5, 1);
 }
 
-//menu buttons
-//et menuRobotics = getElementById("menu-robotics");
-//let menuAbout = getElementById("menu-about-me");
-//let menuGraphics = getElementById("menu-graphics");
-//let menuWeb = getElementById("menu-web");
-//let menuHobby = getElementById("menu-hobby");
-//let menuCad = getElementById("menu-cad");
-//let menuOthers = getElementById("menu-others");
+// menu buttons
+let menuRobotics = document.getElementById("menu-robotics");
+let menuAbout = document.getElementById("menu-about-me");
+let menuGraphics = document.getElementById("menu-graphics");
+let menuWeb = document.getElementById("menu-web");
+let menuHobby = document.getElementById("menu-hobby");
+let menuCad = document.getElementById("menu-cad");
+let menuOthers = document.getElementById("menu-others");
 
+menuRobotics.addEventListener("click", function() {
+    fadeOut(document.getElementById("page-about"));
+    fadeOut(document.getElementById("page-graphics"));
+    fadeOut(document.getElementById("page-web"));
+    fadeOut(document.getElementById("page-hobby"));
+    fadeOut(document.getElementById("page-cad"));
+    fadeOut(document.getElementById("page-others"));
+    fadeIn(document.getElementById("page-robotics"));
+});
 
+menuAbout.addEventListener("click", function() {
+    fadeOut(document.getElementById("page-robotics"));
+    fadeOut(document.getElementById("page-graphics"));
+    fadeOut(document.getElementById("page-web"));
+    fadeOut(document.getElementById("page-hobby"));
+    fadeOut(document.getElementById("page-cad"));
+    fadeOut(document.getElementById("page-others"));
+    fadeIn(document.getElementById("page-about"));
+});
 
+menuGraphics.addEventListener("click", function() {
+    fadeOut(document.getElementById("page-about"));
+    fadeOut(document.getElementById("page-robotics"));
+    fadeOut(document.getElementById("page-web"));
+    fadeOut(document.getElementById("page-hobby"));
+    fadeOut(document.getElementById("page-cad"));
+    fadeOut(document.getElementById("page-others"));
+    fadeIn(document.getElementById("page-graphics"));
+});
+
+menuWeb.addEventListener("click", function() {
+    fadeOut(document.getElementById("page-about"));
+    fadeOut(document.getElementById("page-graphics"));
+    fadeOut(document.getElementById("page-robotics"));
+    fadeOut(document.getElementById("page-hobby"));
+    fadeOut(document.getElementById("page-cad"));
+    fadeOut(document.getElementById("page-others"));
+    fadeIn(document.getElementById("page-web"));
+});
+
+menuHobby.addEventListener("click", function() {
+    fadeOut(document.getElementById("page-about"));
+    fadeOut(document.getElementById("page-graphics"));
+    fadeOut(document.getElementById("page-robotics"));
+    fadeOut(document.getElementById("page-web"));
+    fadeOut(document.getElementById("page-cad"));
+    fadeOut(document.getElementById("page-others"));
+    fadeIn(document.getElementById("page-hobby"));
+});
+
+menuCad.addEventListener("click", function() {
+    fadeOut(document.getElementById("page-about"));
+    fadeOut(document.getElementById("page-graphics"));
+    fadeOut(document.getElementById("page-robotics"));
+    fadeOut(document.getElementById("page-web"));
+    fadeOut(document.getElementById("page-hobby"));
+    fadeOut(document.getElementById("page-others"));
+    fadeIn(document.getElementById("page-cad"));
+});
+
+menuOthers.addEventListener("click", function() {
+    fadeOut(document.getElementById("page-about"));
+    fadeOut(document.getElementById("page-graphics"));
+    fadeOut(document.getElementById("page-robotics"));
+    fadeOut(document.getElementById("page-web"));
+    fadeOut(document.getElementById("page-hobby"));
+    fadeOut(document.getElementById("page-cad"));
+    fadeIn(document.getElementById("page-others"));
+});
+
+//project elements
+let projectContent = document.getElementById("project-content");
+let quadruped = document.getElementById("quadruped");
+
+function showCard(element) {
+    //projectContent
+    element.style.display = "block";
+    //button getElementById
+}
+
+function closeCard() {
+
+}
 
